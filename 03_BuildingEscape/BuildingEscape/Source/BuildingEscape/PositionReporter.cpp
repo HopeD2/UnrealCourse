b9b286b0c9a1dfb5ea98029b63f249ec57783f29
HopeD2 <lobhaspatil@gmail.com>
2019-03-26 01:16:05 +0530

    Report Object Location

--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -8,8 +9,6 @@ UPositionReporter::UPositionReporter()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -18,8 +17,13 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString component = GetOwner()->GetName();
+
+	// The two lines below are basically returning the same thing. Figure out exact difference later on.
+	//FString position = GetOwner()->GetActorLocation().ToString();  
+	FString position = GetOwner()->GetTransform().GetLocation().ToString();    
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at location : %s"), *component, *position);
 }
 
 
