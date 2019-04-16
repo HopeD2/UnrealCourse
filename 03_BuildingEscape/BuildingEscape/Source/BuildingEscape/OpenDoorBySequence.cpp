fae02aed36ee9736bda24c141fdcc8d9d9c2b005
HopeD2 <lobhaspatil@gmail.com>
2019-04-17 01:37:47 +0530

    -Updated level -New Puzzle finally works as expected.

--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoorBySequence.cpp
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoorBySequence.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoorBySequence.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 
 // Sets default values for this component's properties
 UOpenDoorBySequence::UOpenDoorBySequence()
@@ -18,7 +21,37 @@ void UOpenDoorBySequence::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	Owner = GetOwner();
+	if (Owner == nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("No Owner found!"));
+	}
+
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (ActorThatOpens == nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("No Pawn found!"));
+	}
+
+	if (TriggerCapsuleRed == nullptr || TriggerCapsuleOrange == nullptr || TriggerCapsuleYellow == nullptr || TriggerCapsuleGreen == nullptr
+		|| TriggerCapsuleBlue == nullptr || TriggerCapsuleIndigo == nullptr || TriggerCapsuleViolet == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("One of the TriggerCapsule Handle is empty!"));
+	}
+	else {
+		TriggerColorCollection.push_back(TriggerCapsuleViolet);
+		TriggerColorCollection.push_back(TriggerCapsuleIndigo);
+		TriggerColorCollection.push_back(TriggerCapsuleBlue);
+		TriggerColorCollection.push_back(TriggerCapsuleGreen);
+		TriggerColorCollection.push_back(TriggerCapsuleYellow);
+		TriggerColorCollection.push_back(TriggerCapsuleOrange);
+		TriggerColorCollection.push_back(TriggerCapsuleRed);
+	}
+
+	for (size_t i = 0; i < 7; i++)
+	{
+		currentTriggerCollection[i] = false;
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Vector size is : %d"),TriggerColorCollection.size());
 	
 }
 
@@ -28,6 +61,72 @@ void UOpenDoorBySequence::TickComponent(float DeltaTime, ELevelTick TickType, FA
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	bool IsSequenceComplete = false;
+
+	int j = 0;
+
+	if (TriggerCapsuleViolet->IsOverlappingActor(ActorThatOpens)  || currentTriggerCollection[0]) {
+		currentTriggerCollection[j++] = true;
+		if (TriggerCapsuleIndigo->IsOverlappingActor(ActorThatOpens) || currentTriggerCollection[1]) {
+			currentTriggerCollection[j++] = true;
+			if (TriggerCapsuleBlue->IsOverlappingActor(ActorThatOpens) || currentTriggerCollection[2]) {
+				currentTriggerCollection[j++] = true;
+				if (TriggerCapsuleGreen->IsOverlappingActor(ActorThatOpens) || currentTriggerCollection[3]) {
+					currentTriggerCollection[j++] = true;
+					if (TriggerCapsuleYellow->IsOverlappingActor(ActorThatOpens) || currentTriggerCollection[4]) {
+						currentTriggerCollection[j++] = true;
+						if (TriggerCapsuleOrange->IsOverlappingActor(ActorThatOpens) || currentTriggerCollection[5]) {
+							currentTriggerCollection[j++] = true;
+							if (TriggerCapsuleRed->IsOverlappingActor(ActorThatOpens) || currentTriggerCollection[6]) {
+								currentTriggerCollection[j++] = true;
+								IsSequenceComplete = true;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (size_t i = 0; i < TriggerColorCollection.size(); i++)
+	{
+		if (i == j-1) {
+			continue;
+		}
+
+		if (TriggerColorCollection[i]->IsOverlappingActor(ActorThatOpens)) {
+			currentTriggerCollection[j-1] = false;
+			IsSequenceComplete = false;
+		}
+	}
+
+	for (size_t i = 0; i < TriggerColorCollection.size(); i++)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CurrentTriggerCollection[%d] : %d"),i, currentTriggerCollection[i]);
+	}
+
+	if (IsSequenceComplete) {
+		OpenDoor();
+	}
+	else {
+		CloseDoor();
+	}
+	
+}
+
+void UOpenDoorBySequence::OpenDoor()
+{
+	if (!HasDoorOpened) {
+		OnOpen.Broadcast();
+		HasDoorOpened = true;
+	}
+}
+
+void UOpenDoorBySequence::CloseDoor()
+{
+	if (HasDoorOpened) {
+		OnClose.Broadcast();
+		HasDoorOpened = false;
+	}
 }
 
--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoorBySequence.h
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoorBySequence.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerCapsule.h"
+#include <vector>
 #include "OpenDoorBySequence.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorOpenSequenceEvent);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoorBySequence : public UActorComponent
@@ -24,5 +27,47 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	void OpenDoor();
+
+	void CloseDoor();
+
+	UPROPERTY(BlueprintAssignable)
+		FDoorOpenSequenceEvent OnOpen;
+
+	UPROPERTY(BlueprintAssignable)
+		FDoorOpenSequenceEvent OnClose;
+
+private:
+	UPROPERTY(EditAnywhere)
+	ATriggerCapsule *TriggerCapsuleViolet = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerCapsule *TriggerCapsuleIndigo = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerCapsule *TriggerCapsuleBlue = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerCapsule *TriggerCapsuleGreen = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerCapsule *TriggerCapsuleYellow = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerCapsule *TriggerCapsuleOrange = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerCapsule *TriggerCapsuleRed = nullptr;
+
+	ATriggerCapsule *TriggerCapsulePrevious = nullptr;
+
+	std::vector<ATriggerCapsule*> TriggerColorCollection;
+
+	bool currentTriggerCollection[7];
+
+	bool HasDoorOpened = false;
+
+	AActor* Owner = nullptr;
+
+	AActor* ActorThatOpens = nullptr;
 };
